144d704c00e527b1e00072672665fc62c5358e5b
Myron Turner <turnermm02@shaw.ca>
2011-12-11 14:26:56 -0600

    added scrollbars to pop-up window

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
 function QuickstatsShowPage(ns) { 
     var page = DOKU_BASE +'doku.php?&id=' + ns;
-    window.open(page,'quickstats_win',"width=900,height=600");
+    window.open(page,'quickstats_win',"width=900,height=600,scrollbars=yes");
 }
