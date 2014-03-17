9c45303e3b6f0ab66106ba3c75258beca1c66dca
Hideaki SAWADA <chuno@live.jp>
2014-03-17 13:00:36 +0100

    translation update

---
+++ lang/ja/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['excludes']              = '統計情報に加えるべきでない IP アドレスのカンマ区切り一覧';
+$lang['aborts']                = 'サイトにアクセスして欲しくない迷惑 IP アドレスのカンマ区切り一覧';
+$lang['long_names']            = '無制限のために-1に設定され、長い名前を表示する最大文字数。-1 を設定すると無制限。';
+$lang['show_date']             = 'マウスオーバーで、ページの最終アクセスの日付を表示する';
+$lang['rebuild_uip']           = '一意な IP アドレス配列の再作成';
