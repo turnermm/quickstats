ff3b55b90094655626171cee896c58c03a9bebe4
Thor Weinreich <thorweinreich@nefkom.net>
2014-03-18 00:35:06 +0100

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Thor Weinreich <thorweinreich@nefkom.net>
+ */
+$lang['menu']                  = 'Quickstats';
+$lang['btn_delete']            = 'Löschen';
+$lang['btn_restore']           = 'Zurücksetzen';
+$lang['btn_confirm']           = 'Löschen bestätigen';
+$lang['btn_queries']           = 'Daten abfragen';
+$lang['btn_prune']             = 'Quickstats-Cache erneuern';
+$lang['btn_submit_query']      = 'Abfrage übermitteln';
+$lang['btn_qinfo']             = 'Abfrage How-To';
+$lang['label_ip']              = 'IP';
+$lang['label_page']            = 'Seite';
+$lang['label_sel_months']      = 'Monat(e) auswählen (linkes Menü)';
+$lang['label_sel_year']        = 'Gib das Jahr ein';
+$lang['label_current_month']   = 'Aktueller Monat';
+$lang['label_uniq_ip']         = 'Individuelle IPs';
+$lang['label_page_access']     = 'Seiten-Zugriffe';
+$lang['label_total']           = 'Gesamt';
+$lang['label_qs_pages']        = 'Quickstats Seiten';
+$lang['label_date']            = 'Datum';
+$lang['label_search']          = 'IP/Seite/Priorität';
+$lang['btn_close_info']        = 'How-To Fenster schließen';
+$lang['label_no_secondary']    = 'Sekundäre Felder ignorieren';
+$lang['btn_reset']             = 'Zurücksetzen';
+$lang['btn_download']          = 'DB herunterladen';
+$lang['download_fail']         = '%s konnte nicht heruntergeladen werden.';
+$lang['write_fail']            = 'Konnte %s nicht schreiben. Bitte überprüfe die Schreibrechte und/oder den Speicherplatz.';
+$lang['file_saved']            = '%s gespeichert.';
+$lang['no_unpack']             = 'Konnte %s nicht entpacken. Möglicherweise funktioniert es mit einem Archivierungs-Programm auf deinem System.';
+$lang['no_geoip_local']        = 'Die Option "geoip_local" muss aktiviert sein, wenn GeoLiteCity.dat in quickstats/GEOIP/ installiert werden soll.';
---
+++ lang/de/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Thor Weinreich <thorweinreich@nefkom.net>
+ */
+$lang['excludes']              = 'Liste von IPs, welche nicht in der Statistik gezählt werden sollen (durch Kommata getrennt)';
+$lang['aborts']                = 'Liste von lästigen IPs, welche keinen Zugriff auf die Seite erhalten sollen (durch Kommata getrennt)';
+$lang['geoip_local']           = 'Maxmind GeoLiteCity.dat wird sich im Quickstats-Ordner befinden: quickstats/GEOIP';
+$lang['geoip_dir']             = 'Maxmind GeoLiteCity.dat Ordner; Quickstats wird hier nachsehen, falls geoip_local (d.h. quickstats/GEOIP) nicht ausgewählt wurde';
+$lang['geoplugin']             = 'Web-basiertes geoPlugin für Geo-Lokalisierung von IPs verwenden (standardmäßig aktiviert); GeoLiteCity.dat wird nicht benötigt, wenn das hier aktiviert wurde';
+$lang['long_names']            = 'Maximale Anzahl von Zeichen bei der Darstellung langer Namen (-1 für unendlich)';
+$lang['show_date']             = 'Zeige Daten des letzten Seiten-Zugriffs, wenn sich die Maus darüber befindet';
+$lang['show_country']          = 'Zeige zugehöriges Land einer IP in der IP-Tabelle an, wenn sich die Maus darüber befindet. Das funktioniert nur, wenn das "geoplugin" deaktiviert wurde';
+$lang['sorttable_ns']          = 'Liste von ganzen / Teilen von Namensräumen und Dateinamen, welche vor der Ausgabe sortiert werden sollen (durch Kommata getrennt)';
