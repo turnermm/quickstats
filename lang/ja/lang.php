81962bdd29757f6b08312acbdafeb5d0763b4af3
Hideaki SAWADA <chuno@live.jp>
2014-03-18 15:35:30 +0100

    translation update

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -5,6 +5,7 @@
  * 
  * @author Hideaki SAWADA <chuno@live.jp>
  */
+$lang['menu']                  = 'Quickstats';
 $lang['btn_delete']            = '削除';
 $lang['btn_restore']           = 'リセット';
 $lang['btn_confirm']           = '削除の確認';
@@ -28,6 +29,7 @@ $lang['btn_close_info']        = '手引きウインドウを閉じる';
 $lang['label_no_secondary']    = '二次項目を無視';
 $lang['btn_reset']             = 'リセット';
 $lang['btn_download']          = 'DB のダウンロード';
-$lang['download_fail']         = '％s のダウンロードに失敗しました。';
-$lang['write_fail']            = '％s を書き込めません。書込権限やディスクの空き容量を確認して下さい。';
-$lang['file_saved']            = '％s を保存しました。';
+$lang['download_fail']         = '%s のダウンロードに失敗しました。';
+$lang['write_fail']            = '%s を書き込めません。書込権限やディスクの空き容量を確認して下さい。';
+$lang['file_saved']            = '%s を保存しました。';
+$lang['no_geoip_local']        = 'quickstats/GEOIP/ に GeoLiteCity.dat をインストールした場合、"geoip_local" オプションは ON にすべきです。';
--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -7,6 +7,10 @@
  */
 $lang['excludes']              = '統計情報に加えるべきでない IP アドレスのカンマ区切り一覧';
 $lang['aborts']                = 'サイトにアクセスして欲しくない迷惑 IP アドレスのカンマ区切り一覧';
+$lang['geoip_local']           = 'Maxmind GeoLiteCity.dat は quickstats/GEOIP ディレクトリにあります。';
+$lang['geoip_dir']             = 'Maxmind GeoLiteCity.dat のディレクトリ。geoip_local （つまり quickstats/GEOIP）を選択しない場合、ここを探します。';
+$lang['geoplugin']             = 'IP アドレスの位置特定に Web ベースの geoPlugin を使用する（デフォルトは ON）。ON に設定した場合、GeoLIteCity.dat は不要です。';
 $lang['long_names']            = '無制限のために-1に設定され、長い名前を表示する最大文字数。-1 を設定すると無制限。';
 $lang['show_date']             = 'マウスオーバーで、ページの最終アクセスの日付を表示する';
+$lang['max_exec_time']         = 'Quickstats 管理画面での問合せ実行の制限時間（秒）';
 $lang['rebuild_uip']           = '一意な IP アドレス配列の再作成';
