9cea0bbcbf5677d4b12bca08e038644026cd4091
Schplurtz le Déboulonné <schplurtz@laposte.net>
2014-04-26 23:25:08 +0200

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['menu']                  = 'Quickstats';
+$lang['btn_delete']            = 'Supprimer';
+$lang['btn_restore']           = 'Réinitialiser';
+$lang['btn_confirm']           = 'Confirmer les suppressions';
+$lang['btn_queries']           = 'Explorer les données';
+$lang['btn_prune']             = 'Mette à jour le cache Quickstats';
+$lang['btn_submit_query']      = 'soumettre la requête';
+$lang['btn_qinfo']             = 'Manuel des reqêtes';
+$lang['label_ip']              = 'IP';
+$lang['label_page']            = 'Page';
+$lang['label_brief']           = 'affichage court';
+$lang['label_sel_months']      = 'Sélectionner les mois depuis le menu à gauche';
+$lang['label_sel_year']        = 'Saisissez l\'année ci dessous';
+$lang['label_current_month']   = 'Mois courant';
+$lang['label_uniq_ip']         = 'IP uniques';
+$lang['label_page_access']     = 'Accès aux pages';
+$lang['label_total']           = 'Total';
+$lang['label_qs_pages']        = 'Pages quickstats';
+$lang['label_date']            = 'Date';
+$lang['label_search']          = 'IP/Page/priorité';
+$lang['btn_close_info']        = 'Fermer le manuel';
+$lang['label_no_secondary']    = 'Ignorer les champs secondaires';
+$lang['btn_reset']             = 'Réinitiailser';
+$lang['btn_download']          = 'Télécharger la BDD';
+$lang['download_fail']         = 'Échec du téléchargement de %s';
+$lang['write_fail']            = 'Impossible d\'écrire %s. Veuillez vérifier vos permissions et/ou l\'espace disque.';
+$lang['file_saved']            = '%s enregistré.';
+$lang['no_unpack']             = 'Ne peut décompresser %s. Vous pouvez peut-être le faire avec un utilitaire zip sur votre système.';
+$lang['no_geoip_local']        = 'L\'option "geoip_local" doit être activée pour installer GeoLiteCity.dat dans quickstats/GEOIP/ ';
+$lang['click_to_view']         = 'Cliquez pour voir le fichier';
+$lang['user_agent']            = 'Agent Utilisateur';
+$lang['sel_user_agent']        = 'Choisissez l\'agent utilisateur';
+$lang['country']               = 'Pays';
+$lang['sel_country']           = 'Choisissez le pays';
+$lang['search_link']           = 'Chercher:';
+$lang['year']                  = 'Année:';
+$lang['browsers']              = 'Navigateur';
+$lang['countries']             = 'Pays';
+$lang['platforms']             = 'Plateformes';
+$lang['browsers_and_ua']       = 'Navigateur et agents utilisateurs&nbsp;';
+$lang['uniq_ip']               = 'adresses IP uniques';
+$lang['ttl_uniq_ip']           = 'Total d\'adresses IP uniques&nbsp;';
+$lang['ttl_accesses_ua']       = 'Total des accès par agent utilisateur';
+$lang['accesses']              = 'Accès';
+$lang['pages_accessed']        = 'Nombre de pages vues:&nbsp;';
+$lang['ttl_countries']         = 'Nombre total de pays:&nbsp;';
+$lang['ttl_accesses']          = 'Total des accès:&nbsp;';
---
+++ lang/fr/settings.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['excludes']              = 'Liste séparée par des virgules des adresses IP qui ne doivent pas être comptées dans les statistiques';
+$lang['aborts']                = 'Liste séparée par des virgules des adresses IP nuisibles qui ne devraient pas avoir accès au site';
+$lang['geoip_local']           = 'Le fichier Maxmind GeoLiteCity.dat se trouve dans le dossier quickstats:
+quickstats/GEOIP';
+$lang['geoip_dir']             = 'Dossier du fichier Maxmind GeoLiteCity.dat. Quickstats le recherchera dans ce dossier si geoip_local (i.e. quickstats/GEOIP) n\'est pas sélectionné.';
+$lang['geoplugin']             = 'Utiliser l\'extension web geoPlugin pour géolocaliser les IP (actif par défaut). Quand ce réglage est actif, GeoLIteCity.dat est inutile.';
+$lang['long_names']            = 'Longueur maximale pour l\'affichage des noms longs. Régler sur -1 pour ne pas limiter';
+$lang['show_date']             = 'Afficher la date de dernier accès aux pages lors du survol par le curseur';
+$lang['show_country']          = 'Dans le tableau IP, afficher le pays de l\'IP lors du survol par le curseur. Ceci ne fonctionne que lorsque "geoplugin" est désactivé';
+$lang['sorttable_ns']          = 'Liste séparée par des virgules des noms partiels ou complets de catégories et noms de fichier qui nécessitent un affichage triable';
+$lang['xcl_name_val']          = 'Liste séparée par des virgules des paires nom/valeur à l\'interieur des requêtes web qui devraient être exclues des statistiques';
+$lang['max_exec_time']         = 'Durée maximum en secondes des requêtes dans le panneau d\'administration quickstats';
+$lang['rebuild_uip']           = 'Reconstruire le tableau des IP uniques';
