93069232e818320a4e972543bde83fc47e5cabec
Myron Turner <turnermm02@shaw.ca>
2015-12-23 07:59:25 -0600

    brought <action>::register declaration into conformity with PHP 7.0

--- action.php
+++ action.php
@@ -89,7 +89,7 @@ class action_plugin_quickstats extends DokuWiki_Action_Plugin {
         /**
      * Register its handlers with the DokuWiki's event controller
      */
-    function register(&$controller) {
+    function register(Doku_Event_Handler $controller) {
     
        $controller->register_hook('DOKUWIKI_STARTED', 'BEFORE', $this, 'set_cookies');
        $controller->register_hook('DOKUWIKI_STARTED', 'AFTER', $this, 'search_queries');              
