a37da12d411e3ddba53c8f168380929fd2c07b0b
Myron Turner <turnermm02@shaw.ca>
2019-11-01 09:42:02 -0500

    additions to fnmach.php

--- scripts/fnmach.php
+++ scripts/fnmach.php
@@ -1,52 +1,59 @@
 <?php
 if(!defined('DOKU_INC')) define('DOKU_INC', realpath(dirname(__FILE__) .'/../../') . '/');
 define ('TEMPDIR', DOKU_INC . 'data/tmp/');
-echo DOKU_INC . "\n";
+//echo DOKU_INC . "\n";
 
 $ro = ini_get('phar.readonly');
 echo $ro . "\n";
 if($ro) ini_set('phar.readonly','0');
-
-$files = scandir(TEMPDIR);
-foreach ($files as $file) {
-      echo $file ."\n";
-      if (preg_match("#GeoLite2-City.tar.gz#", $file,$matches)) { 
-       $p = new PharData($file);
-       $p->decompress(); // creates /path/to/my.tar
-       $tar = str_replace('.gz', "", $file);       
-        echo $tar . "\n";
-        try {
-             $phar = new PharData($tar);
-             $phar->extractTo('./tmp'); // extract all files
-        } catch (Exception $e) {
-            echo $e->getMessage() . "\n";
-        }
-      
-            $dir =TEMPDIR . 'tmp/';
-            $geodir = scandir($dir );
-            foreach($geodir  as $entry) {
-                 echo $dir . $entry . "\n";
-                if(preg_match("#\.mmdb$#",$entry)) {
-                   echo $dir . $entry . "\n";
-                }
+function unpack_geocity2() {
+    $files = scandir(TEMPDIR);
+    foreach ($files as $file) {
+          echo TEMPDIR.$file ."\n";
+          if (preg_match("#GeoLite2-City.tar.gz#", $file,$matches)) { 
+           $p = new PharData(TEMPDIR.$file);
+           $p->decompress(); // creates /path/to/my.tar
+           $tar = str_replace('.gz', "", $file);       
+            echo $tar . "\n";
+            try {
+                 $phar = new PharData(TEMPDIR.$tar);
+                 $phar->extractTo(TEMPDIR.'tmp'); // extract all files
+            } catch (Exception $e) {
+                echo $e->getMessage() . "\n";
             }
-     
+        }
     }
 }
 
-function delete_gcity() {
-    $tmpdir_files = scandir('./tmp');
-//print_r($tmpdir_files ); 
-foreach ($tmpdir_files as $tmpfile) {
-      if(preg_match("#(?i)GeoLite2-City_\d+#",$tmpfile) ){
-          echo $tmpfile . "\n";
-          if(is_dir(TEMPDIR . 'tmp/'. $tmpfile)) {
-              echo TEMPDIR . 'tmp/'. $tmpfile . " is a directory\n";
-          }
-          else echo TEMPDIR . 'tmp/'. $tmpfile . " is NOT a directory\n";
-          break;
-      }
-}
+function process_gcity() {   
+   $tmpdir_files = scandir(TEMPDIR . 'tmp'); 
+    foreach ($tmpdir_files as $tmpfile) {
+         $current_file = TEMPDIR . "tmp/${tmpfile}";
+          if(preg_match("#(?i)GeoLite2-City_\d+#",$tmpfile) ) {           
+              if(is_dir(TEMPDIR . 'tmp/'. $tmpfile)) {                  
+                  $geo_dir_name =  TEMPDIR . 'tmp/'. $tmpfile;
+                  echo "$geo_dir_name is a geocity directory\n";
+                  $geo_dir = scandir(TEMPDIR . 'tmp/'. $tmpfile);                
+                  echo "Directory name: $geo_dir_name\n";              
+                  foreach($geo_dir as $gfile) {
+                      if(!is_dir(TEMPDIR . 'tmp/'. $gfile)) {                          
+                          if(preg_match("/\.mmdb$/",$gfile)) {                           
+                              echo "renaming " . $geo_dir_name. "/$gfile" ."\nTo: " . TEMPDIR . $gfile ."\n";
+                             rename($geo_dir_name. "/$gfile" , TEMPDIR . $gfile ) ;
+                             continue;
+                          }    
+                           echo "$gfile\n";
+                      }
+                     
+                  }
+               //   break;
+              }
+              else {
+                  echo TEMPDIR . 'tmp/'. $tmpfile . " is NOT a directory\n";                                
+              }
+          }      
+    }
  
 }
+process_gcity();
 ?>
